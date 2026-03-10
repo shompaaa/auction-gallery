e998b810da6ee087319d82792dc480add2ed8d64
shompaaa <shompaakter334@gmail.com>
2026-03-10 16:27:24 +0600

    show items in the table & design

--- src/components/Auction/Auction.jsx
+++ src/components/Auction/Auction.jsx
@@ -1,22 +1,32 @@
 import React from "react";
+import { FaRegHeart } from "react-icons/fa";
 
 const Auction = ({ auction }) => {
   const { image, title, currentBidPrice, timeLeft } = auction;
   return (
     <div>
-      <table className="table">
-        <tbody>
-          <tr>
-            <td>{image}</td>
-            <td>{title}</td>
-            <td>${currentBidPrice}</td>
-            <td>{timeLeft}</td>
-            <td>
-              <button>X</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="overflow-x-auto">
+        <table className="table">
+          <tbody>
+            {/* row 1 */}
+            <tr className="text-start">
+              <td className="w-4/7">
+                <div className="flex items-center gap-3">
+                  <img className="h-24 w-28 rounded-xl" src={image} alt="" />
+                  <div className="font-bold">{title}</div>
+                </div>
+              </td>
+              <td> ${currentBidPrice}</td>
+              <td>{timeLeft}</td>
+              <td>
+                <button>
+                  <FaRegHeart size={20} />
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
--- src/components/Auctions/Auctions.jsx
+++ src/components/Auctions/Auctions.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Auction from "../Auction/Auction";
+import { FaRegHeart } from "react-icons/fa";
+
 
 const Auctions = () => {
   const [auctions, setAuctions] = useState([]);
@@ -11,33 +13,45 @@ const Auctions = () => {
   }, []);
 
   return (
-    <div className="mx-32 my-32">
-      <div>
+    <div className="mx-32 my-32 flex justify-between gap-10">
+      <div className="w-3/4">
         <h2 className="text-3xl mb-3">Active Auctions</h2>
         <p className="text-sm mb-10 text-gray-600">
           Discover and bid on extraordinary items
         </p>
-        <table className="table">
-          <thead>
-            <th>
-              <td>Items</td>
-            </th>
-            <th>
-              <td>Current Bid</td>
-            </th>
-            <th>
-              <td>Times Left</td>
-            </th>
-            <th>
-              <td>Bid Now</td>
-            </th>
-          </thead>
-        </table>
+        <div className="bg-white p-5 rounded-xl">
+            <div className="flex justify-between mb-3 font-bold">
+            <div className="w-[75%]">Items</div>
+            <div className="flex justify-around w-3/4">
+                <p>Current Bid</p>
+                <p>Time Left</p>
+                <p>Bid Now</p>
+            </div>
+        </div>
+        <hr className="text-gray-300" />
         {auctions.map((auction) => (
           <Auction auction={auction}></Auction>
         ))}
+        </div>
       </div>
-      <div></div>
+
+      {/* Right Side Content */}
+      <div className="mt-27 bg-white p-5 rounded-xl h-full">
+        <div className="flex items-center px-5 mb-2 justify-center gap-3">
+            <FaRegHeart size={22} />
+            <h2 className="text-2xl text-[#0E2954] font-semibold">Favorite Items</h2>
+        </div>
+        <hr className="text-gray-200" />
+        <div className="px-20 py-8 text-center">
+            <h2 className="font-bold">No favorites yet</h2>
+            <p className="text-gray-400">Click the heart icon on any item to add it to your favorites</p>
+        </div>
+        <hr className="text-gray-200 mb-2" />
+        <div className="flex justify-between mx-5 font-bold">
+            <h2>Total bids Amount</h2>
+            <h2>$0000</h2>
+        </div>
+      </div>
     </div>
   );
 };
